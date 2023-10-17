731de529e7629d169f92f6b0ea73ab98f5b9f463
Srinivas Koduru <skoduru2708@gmail.com>
2023-10-17 07:06:49 -0400

    tracker added

--- ch6.cpp
+++ ch6.cpp
@@ -6,32 +6,68 @@
 using namespace cv;
 using namespace std;
 
+string getOsName();
+
 /// Color detection /////
 int main(int Args, char *Arguments[], char *Env[])
 {
-    int Index = 0;
-    cout << "SK Debug: Start" << endl;
-    while (Env[Index] != NULL)
+    string img_path;
+
+    if (getOsName() == "MacOSX")
     {
-        cout << Env[Index] << endl;
-        Index++;
+        img_path = "../Resources/";
     }
-    cout << "SK Debug: End" << endl;
-    string img_path = "../../Resources/lambo.png";
-    int hmin = 0, smin = 110, vmin = 153;
-    int hmax = 19, smax = 240, vmax = 255;
+    else if (getOsName() == "Win")
+    {
+        img_path = "../../Resources/";
+    }
+    img_path += "/shapes.png";
+
+    int hmin = 0, smin = 0, vmin = 0;
+    int hmax = 179, smax = 240, vmax = 255;
+
     Mat img, img_HSV, img_mask;
-    Scalar lower(hmin, smin, vmin);
-    Scalar upper(hmax, smax, vmax);
 
     img = imread(img_path);
     cvtColor(img, img_HSV, COLOR_BGR2HSV);
 
-    inRange(img_HSV, lower, upper, img_mask);
+    namedWindow("Trackbars", (640, 200));
+    createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+    createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+    createTrackbar("Sat Min", "Trackbars", &smin, 255);
+    createTrackbar("Sat Max", "Trackbars", &smax, 255);
+    createTrackbar("Val Min", "Trackbars", &vmin, 255);
+    createTrackbar("Val Max", "Trackbars", &vmax, 255);
 
-    imshow("Image", img);
-    imshow("HSV Image", img_HSV);
-    imshow("Masked Image", img_mask);
-    waitKey(0);
+    while (true)
+    {
+        Scalar lower(hmin, smin, vmin);
+        Scalar upper(hmax, smax, vmax);
+        inRange(img_HSV, lower, upper, img_mask);
+
+        imshow("Image", img);
+        imshow("HSV Image", img_HSV);
+        imshow("Masked Image", img_mask);
+        waitKey(1);
+    }
     return 0;
+}
+
+string getOsName()
+{
+#ifdef _WIN32
+    return "Win";
+#elif _WIN64
+    return "Win";
+#elif __APPLE__ || __MACH__
+    return "MacOSX";
+#elif __linux__
+    return "Linux";
+#elif __FreeBSD__
+    return "FreeBSD";
+#elif __unix || __unix__
+    return "Unix";
+#else
+    return "Other";
+#endif
 }
